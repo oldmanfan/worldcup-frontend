bd09eed589b7b683fd98994c963ed610c2a04cfd
enpeng <enpenguc@163.com>
2022-11-13 15:06:29 +0800

    feat: moment

--- packages/mgt/src/utils/index.ts
+++ packages/mgt/src/utils/index.ts
@@ -1,15 +1,17 @@
 import moment from 'moment';
 
 export function getUtcTime(day: moment.Moment) {
-  const str = day.format('YYYY-MM-DD HH:mm:ss').replace(' ', 'T') + 'Z';
-  return parseInt ((new Date(str).getTime() / 1000).toString(), 10);
+  // const str = day.format('YYYY-MM-DD HH:mm:ss').replace(' ', 'T') + 'Z';
+  // return parseInt((new Date(str).getTime() / 1000).toString(), 10);
+  return parseInt((day.valueOf() / 1000).toString(), 10);
 }
 
 export function toUtcTime(time: number) {
   if (!time) {
     return '--';
   }
-  return new Date(time * 1000).toISOString().replace('T', ' ').replace('.000Z', '');
+  // return new Date(time * 1000).toISOString().replace('T', ' ').replace('.000Z', '');
+  return moment(time * 1000).format('YYYY-MM-DD HH:mm:ss');
 }
 
 export function getErrorMsg(err: any) {
